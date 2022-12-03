7971196fc15cfc49f821bf1144d7272ff4f07ab0
Souvik Mitra <mitrasouvik123@gmail.com>
2022-12-03 13:01:00 +0530

    added home page and styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import TypewriterComponent from 'typewriter-effect';
+import { BsArrowUpRight, BsChevronDoubleDown } from 'react-icons/bs'
+import mypic from '../Assets/mypic.jpg'
 
 const Home = () => {
 
@@ -44,16 +46,46 @@ const Home = () => {
                             strings: ["A Developer", "A Musician", "A Learner"],
                             autoStart: true,
                             loop: true,
-                            cursor: "",
                             wrapperClassName: "typewriterpara",
                         }}
 
                     />
 
+                    <div>
+                        <a href="mailto: mitrasouvik123@gmail.com">Hire Me!</a>
+                        <a href="#work">Projects <BsArrowUpRight /></a>
+
+
+                    </div>
+
+
+                    <aside>
+                        <article>
+                            <p>
+                                +<span>10</span>
+                            </p>
+                            <span>Projects built</span>
+                        </article>
+
+                        <article data-special>
+                            <p>
+                                Contact
+                            </p>
+                            <span>mitrasouvik123@gmail.com</span>
+                        </article>
+                    </aside>
+
 
                 </div>
             </section>
-            <section></section>
+            <section>
+
+                <img src={mypic} alt="myPic" />
+
+
+            </section>
+
+            <BsChevronDoubleDown />
 
         </div>
     )
